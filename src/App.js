e0dc1ee557a8df62bf80d8c145bc635676e5c947
Manas1255 <anas1255.ma@gmail.com>
2024-11-19 19:16:21 +0500

    feat: SideBar completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import HomePage from "./pages/HomePage";
 import ProcessPage from "./pages/ProcessPage";
+import SideBar from "./components/SideBar";
 
 const App = () => {
   return (
     <div>
       {/* <HomePage /> */}
-      <ProcessPage />
+      {/* <ProcessPage /> */}
+      <SideBar />
     </div>
   );
 };
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,5 +1,71 @@
 import React from "react";
+import HistoryIcon from "../assets/history-icon.svg";
+import SettingsIcon from "../assets/settings-icon.svg";
+import UserIcon from "../assets/user-icon.svg";
+import "bootstrap/js/dist/dropdown";
 
-export const NavBar = () => {
-  return <div>NavBar</div>;
+const SideBar = () => {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="bg-dark col-auto col-md-1 min-vh-100 d-flex flex-column align-items-center justify-content-center">
+          <div>
+            <ul className="nav nav-pills flex-column">
+              <li className="nav-item">
+                <a href="#" className="nav-link active" aria-current="page">
+                  <img
+                    src={HistoryIcon}
+                    className="img-fluid"
+                    style={{ width: "35px", height: "35px" }}
+                  />
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#" className="nav-link">
+                  <img
+                    src={SettingsIcon}
+                    className="img-fluid"
+                    style={{ width: "35px", height: "35px" }}
+                  />
+                </a>
+              </li>
+              <li className="nav-item disabled">
+                <a href="#" className="nav-link">
+                  Disabled
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="dropdown dropup">
+            <a
+              class=" dropdown-toggle"
+              type="button"
+              id="triggerId"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <img
+                src={UserIcon}
+                className="img-fluid"
+                style={{ width: "35px", height: "35px" }}
+              />
+            </a>
+            <div class="dropdown-menu" aria-labelledby="triggerId">
+              <a class="dropdown-item" href="#">
+                Settings
+              </a>
+              <a class="dropdown-item " href="#">
+                Account
+              </a>
+              <a class="dropdown-item " href="#">
+                Logout
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
+
+export default SideBar;
